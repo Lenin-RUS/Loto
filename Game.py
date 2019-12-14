d222f7716450d52e29d8220e9597b82ba40fccb3
Vivo <S.Lanin@gmail.com>
2019-12-15 00:07:55 +0300

    magic

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class meshok:
+class Meshok:
     def __init__(self):
         numbers = np.arange(1, 91)
         np.random.shuffle(numbers)
@@ -11,8 +11,14 @@ class meshok:
     def get_barrel(self, next_barrel):
         self.current_barrel = self.numbers[next_barrel]  # <---- Чет перебор
 
+    def __str__(self):
+        return f'Номера бочонков по порядку вытаскивания:{self.numbers}'
 
-class card:
+    def __len__(self):
+        return len(self.numbers)
+
+
+class Card:
     def __init__(self, user):
         numbers = np.arange(1, 91)
         np.random.shuffle(numbers)
@@ -57,18 +63,33 @@ class card:
                 print(f'{Player.name}, ты слепой? Ты проиграл кароч.')
                 Player.player_status = 0
 
+    # def __str__(self):
+    #     return f'Номера на карточке игрока {self.user}: {self.numbers}'
+    def __eq__(self, other):
+        return self.user == other.user
 
-class user:
+
+
+class User:
     def __init__(self, name, player_type):
         self.name = name
         self.player_type = player_type
         self.player_status = 1
+    def __str__(self):
+        return f'имя пользователя {self.name}, тип пользователя: {"Компьютер" if self.player_type == 1 else "человек"}'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+
+
 
 if __name__ == '__main__':
 
     print('__________________ (((СУПЕР ИГРА ЛОТО )))_______________________')
     max_players = int(input('Введите количество игроков: '))
-    Players = []
+    players = []
     cards = []
     winners = []
     # заводим игроков и для них карточки
@@ -79,13 +100,25 @@ if __name__ == '__main__':
         while Player_type not in (0, 1):
             print('Тип пользователя неверный. Попробуйте еще раз')
             Player_type = int(input(f'Введите тип игрока {Player_name} (1-комп, 0-юзер): '))
-        Players.append(user(Player_name, Player_type))
-        cards.append(card(Player_name))
+        players.append(User(Player_name, Player_type))
+        cards.append(Card(Player_name))
         cards[i].show_card()
+
+    # print(cards(Players[0].name))   <----- Не сработает
+
 
     # мутим мешок
 
-    our_bag = meshok()
+    our_bag = Meshok()
+
+
+    print('Данные первого пользователя', players[0])
+    print(our_bag)
+    print(players[0]==players[1])
+    print(cards[0]!=cards[1])
+    print('Количество боченоков в мешке - ', len(our_bag))
+
+
 
     print('__________________ ((((НАЧАЛО ЗАМЕСА))))_______________________')
     winner_defined = False
@@ -96,13 +129,13 @@ if __name__ == '__main__':
         print('Мешаем мешок и достаем бочонок с номером', barrel)
 
         # сперва идем по игрокам юзерам
-        for Player_Card, Player in enumerate(Players):
+        for Player_Card, Player in enumerate(players):
             if Player.player_type == 0 and Player.player_status == 1:
                 print(f'----------------------------------Ход игрока {Player.name}')
                 cards[Player_Card].check_number(barrel, Player)
 
         # потом идем по игрокам компам
-        for Player_Card, Player in enumerate(Players):
+        for Player_Card, Player in enumerate(players):
             if Player.player_type == 1 and Player.player_status == 1:
                 print(f'--------------------------------- Ход компа {Player.name}')
                 cards[Player_Card].check_number(barrel, Player)
@@ -112,7 +145,7 @@ if __name__ == '__main__':
         for j, loto_card in enumerate(cards):
             print(f'Игорок {loto_card.user} - осталось {len(loto_card.numbers.nonzero()[0])} незакрытых позиций')
             if len(loto_card.numbers.nonzero()[0]) == 0:
-                Players[j].player_status = 2
+                players[j].player_status = 2
                 winners.append(loto_card.user)
         if len(winners) == 1:
             print(f'У нас победитель - Игорок {winners[0]}!!! \n Игра окончена!!!')
--- test_pytest_loto.py
+++ test_pytest_loto.py
@@ -1,46 +1,58 @@
-from game import meshok, card, user
+from Game import Meshok, Card, User
 
 
 class TestMeshok:
 
     def test_meshok_init(self):
-        new_bag=meshok()
+        new_bag=Meshok()
         assert new_bag.current_barrel==0
         assert len(new_bag.numbers)==90
 
     def test_meshok_get_barrel(self):
-        new_bag=meshok()
+        new_bag=Meshok()
         new_bag.get_barrel(5)
         assert new_bag.current_barrel in new_bag.numbers
+
+    def len_test(self):
+        new_bag = Meshok()
+        assert len(new_bag) == 90
 
 class TestCard:
 
     def test_card_init(self):
         tmp_player='Иван'
-        new_card=card(tmp_player)
+        new_card=Card(tmp_player)
         assert len(new_card.numbers)==27
         assert new_card.state == 1
         assert new_card.user == tmp_player
 
     def test_show_card(self):
         tmp_player='Иван'
-        new_card=card(tmp_player)
+        new_card=Card(tmp_player)
         assert new_card.show_card()==1
 
     def test_check_number(self):
-        new_bag=meshok()
+        new_bag=Meshok()
         tmp_player_name='Иван'
-        new_card=card(tmp_player_name)
-        tmp_player=user(tmp_player_name, 1)
+        new_card=Card(tmp_player_name)
+        tmp_player=User(tmp_player_name, 1)
         for i in new_bag.numbers:
             new_card.check_number(i, tmp_player)
         # должны получиться одни 0
         assert sum(new_card.numbers)==0
 
+    def test_eq_card(self):
+        tmp_player = 'Иван'
+        new_card_1 = Card(tmp_player)
+        new_card_2 = Card(tmp_player)
+        assert new_card_1==new_card_2
+
+
+
 class TestUser:
     def test_user_init(self):
         tmp_player_name='Иван'
-        tmp_player=user(tmp_player_name, 1)
+        tmp_player=User(tmp_player_name, 1)
         assert tmp_player.name==tmp_player_name
         assert tmp_player.player_status == 1
         assert tmp_player.player_type == 1
